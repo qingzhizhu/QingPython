197bfa5a30bd43a9c7843651787ea76bbfe86788
gengkun <gengkun123@gmail.com>
2018-08-08 19:52:31 +0800

    梯度下降dmeo

---
+++ asset_demo.py
@@ -0,0 +1,20 @@
+def remove_html_markup(s):
+    tag   = False
+    quote = False
+    out   = ""    
+
+    for c in s:
+        assert not tag
+        if c == '<' and not quote:
+            tag = True
+        elif c == '>' and not quote:
+            tag = False
+        elif c == '"' or c == "'" and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+
+    return out
+
+remove_html_markup("<FOO")
+
