244ebcae64bca689aa81bc906614fa41260f4f01
gengkun <gengkun@elex-tech.com>
2018-09-21 17:33:59 +0800

    输出当前工作目录

--- template.py
+++ template.py
@@ -8,15 +8,26 @@ import os,sys
 DIR=os.getcwd()
 
 def foo():
-	print("中文,Done!")
+    print("中文,Done!")
+    print(DIR)
 
 
 if __name__ == "__main__":
-	# for arg in sys.argv:  
-	# 	print(arg)
+    # for arg in sys.argv:  
+    # 	print(arg)
 
-	print("file:", sys.argv[0])
-	for i in range(1, len(sys.argv)):
-		print("param:", i, sys.argv[i])
+    # print("file:", sys.argv[0])
+    # for i in range(1, len(sys.argv)):
+    # 	print("param:", i, sys.argv[i])
+    fullName = sys.argv[0]
+    print("file:", fullName)
 
-	foo()
+    if fullName[0] == '/' :
+        # print('切换目录')
+        os.chdir(os.path.abspath(os.path.dirname(fullName)))
+        DIR=os.getcwd()
+
+    for i in range(1, len(sys.argv)):
+        print("参数", i, sys.argv[i])
+
+    foo()
